591b1ecfbd89f3d257c0d0904eb8700d50ac0d5c
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-01-28 23:22:44 -0300

    Java Web Application with Spring Framework and JSP

---
+++ src/test/java/io/github/matheusgit11/MyFirstWebApp/MyFirstWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.matheusgit11.MyFirstWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
