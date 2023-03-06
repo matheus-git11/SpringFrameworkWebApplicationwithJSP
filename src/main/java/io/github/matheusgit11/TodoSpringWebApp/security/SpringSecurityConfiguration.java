a711eb2d1b6b08b397fcddd00cee66c6a3b2510d
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-03-06 17:50:56 -0300

    Configuring spring security with h2 database , adding pom.xml Jpa and h2 dependencies and start do make Todo a entity

--- src/main/java/io/github/matheusgit11/TodoSpringWebApp/security/SpringSecurityConfiguration.java
+++ src/main/java/io/github/matheusgit11/TodoSpringWebApp/security/SpringSecurityConfiguration.java
@@ -2,21 +2,26 @@ package io.github.matheusgit11.TodoSpringWebApp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
     //LDAP or Database or in memory
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager(){
-        UserDetails userDetails1 = createNewUser("matheus", "123");
-        UserDetails userDetails2 = createNewUser("ranga", "456");
+        UserDetails userDetails1 = createNewUser("matheus", "matheus11");
+        UserDetails userDetails2 = createNewUser("visitor", "123");
         return new InMemoryUserDetailsManager(userDetails1,userDetails2);
     }
 
@@ -38,5 +43,26 @@ public class SpringSecurityConfiguration {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    //all Urls are protected
+    // a login form is shown for unauthorized requests
+    //CSRF disable
+    //Frames
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+
+        httpSecurity.formLogin(withDefaults());
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
+    }
+
+
+
+
+
 
 }
